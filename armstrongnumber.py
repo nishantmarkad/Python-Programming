76a674ab30383f5b0057b80808e52359f805e3b6
nishantmarkad <43986216+nishantmarkad@users.noreply.github.com>
2018-10-09 15:42:22 +0530

    Update armstrongnumber.py

--- armstrongnumber.py
+++ armstrongnumber.py
@@ -3,9 +3,9 @@ def armstrong_number(x):
     sum=0
     num=x 
     while x>0:
-        digit=x%10
+        digit=x%10           #153%10 gives 3
         sum=digit**3+sum
-        x=x//10              #because we have to cube each number
+        x=x//10              #because we have to cube each number  153//10 gives 15
     print(sum)
     if sum==num:
        print(" Armstrong number")
