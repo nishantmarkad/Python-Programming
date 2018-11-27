f5835e8f7f6bf5daae090acc71e491c5664954af
nishantmarkad <43986216+nishantmarkad@users.noreply.github.com>
2018-11-27 14:41:38 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,13 +1,22 @@
+#FACTORIAL OF A NUMBER.
+#NISHANT MARKAD
+#DIV-M,ROLL NO-39
+
+
 print("Factorial(!)")
 print("Enter a number")
-x=int(input())
-product=1
+
+
+x=int(input())           #user input
+product=1                #initialize
 if x==0:
     product=1
 if x==1:
     product=1
-while x>1:
+while x>1:               #while loop 
     product=product*x
-    x=x-1
-print("Factorial is ",product)    
-m=input("\n press the enter key to exit")
+    x=x-1                #updating x
+print("Factorial is ",product) 
+
+
+
