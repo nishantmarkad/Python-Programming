4125023e7c73e945265ba94578e9f14660917b9e
nishantmarkad <43986216+nishantmarkad@users.noreply.github.com>
2018-11-27 15:00:47 +0530

    Update square.py

--- square.py
+++ square.py
@@ -2,8 +2,8 @@
 #NISHANT MARKAD
 #DIV-M,ROLL NO-39
 
-
-for i in range(1,10);              #for loop
+#change this value(10) for different result.
+for i in range(1,10);              #for loop   
     square=i*i
     print("the square of i is",square)
 print("end of program")
