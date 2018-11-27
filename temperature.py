3b89474c4d7557bd545e4390b3764107731d5759
nishantmarkad <43986216+nishantmarkad@users.noreply.github.com>
2018-11-27 15:06:30 +0530

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,8 @@
+x=int(input("Enter temperature:"))                       #input from user
+y=input("is it in 'celcius' or 'farenhite'")
+if y is 'celsius':                                       #if-else statement
+ f=9*x/5 + 32
+ print(f)
+else:
+ c=5/9*(x-32)
+print(c)
