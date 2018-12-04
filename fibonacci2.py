d4f6aaf6953b816b5c23a687c4e5caa4fa5f6322
nishantmarkad <43986216+nishantmarkad@users.noreply.github.com>
2018-12-04 10:28:27 +0530

    Update fibonacci2.py

--- fibonacci2.py
+++ fibonacci2.py
@@ -4,15 +4,12 @@
 
 # change this value for a different result
 nterms = 10
-
 # uncomment to take input from the user
 #nterms = int(input("How many terms? "))
-
 # first two terms
 n1 = 0
 n2 = 1
 count = 0
-
 # check if the number of terms is valid
 if nterms <= 0:
    print("Please enter a positive integer")
