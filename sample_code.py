94244929c6c200531c41b78aff87d73e3d762254
nishantmarkad <43986216+nishantmarkad@users.noreply.github.com>
2018-12-20 12:46:34 +0530

    Create sample_code.py

---
+++ sample_code.py
@@ -0,0 +1,25 @@
+#course project
+
+
+from easygui import diropenbox, buttonbox
+import glob
+state = True
+j=0
+path = diropenbox(msg=None, title=None, default='./folder path')
+message = 'Image Viewer - Using Easygui'
+choice = ('Previous', 'Next', 'Exit')
+image_set = glob.glob(path + '/*.jpg')                                                          # can take any image extension
+choice_select = buttonbox(msg=message, title=' ', choices=choice, image=image_set[0])
+while state:
+    if choice_select == 'Next':
+        if j< (len(image_set)-1):
+            j=j+1
+            choice_select = buttonbox(msg=message, title=' ', choices=choice, image=image_set[j])
+        else:
+            j=0
+            choice_select = buttonbox(msg=message, title=' ', choices=choice, image=image_set[j])
+    if choice_select == 'Previous':
+        j-=1
+        choice_select = buttonbox(msg=message, title=' ', choices=choice, image=image_set[j])
+    if choice_select == 'Exit':
+        state = False
